4423a72e68b13ccddb86283458e2cb0dc8e28271
vyasdhyey <122445963+vyasdhyey@users.noreply.github.com>
2024-03-16 10:55:21 +0530

    Update assessment.sql

--- assessment.sql
+++ assessment.sql
@@ -41,3 +41,32 @@ WHERE W.salary IN(
     FROM WORKER
     WHERE DEPARTMENT = W.DEPARTMENT
 );
+
+-- Question 2: Open school database, then select student table and use following SQL statements.
+-- TYPE THE STATEMENT, PRESS ENTER AND NOTE THE OUTPUT
+create database school;
+use school;
+
+create table student(StdID int,StdName varchar(20),Sex varchar(20),Percentage int,Class int,Sec varchar(5),Stream varchar(50),DOB date);
+insert into student(StdID,StdName,Sex,Percentage,Class,Sec,Stream,DOB) 
+values(1001,'Surekha Joshi','Female',82,12,'A','Science','1998/08/03');
+insert into student(StdID,StdName,Sex,Percentage,Class,Sec,Stream,DOB) 
+values
+(1002,'Maahi Agrawal','Female',56,11,'C','Commerce','2008/11/23'),
+(1003,'Sanam Verma','Male',59,11,'C','Commerce','2006/06/29'),
+(1004,'Ronit Kumar','Male',63,11,'C','Commerce','1997/11/05'),
+(1005,'Dipesh Pulkit','Male',78,11,'B','Science','2003/09/14'),
+(1006,'Jahanvi Puri','Female',60,11,'B','Commerce','2008/11/07'),
+(1007,'Sanam Kumar','Male',23,12,'F','Commerce','1998/08/03'),
+(1008,'Sahil saras','Male',56,11,'C','Commerce','2008/11/07'),
+(1009,'Akshra Agrawal','Female',72,12,'B','Commerce','1996/10/01'),
+(1010,'Stuti Mishra','Female',39,11,'F','Science','2008/11/23'),
+(1011,'Harsh Agrawal','Male',42,11,'C','Science','1998/08/03'),
+(1012,'Nikunj Agrawal','Male',49,12,'C','Commerce','1998/08/28'),
+(1013,'Akriti Saxena','Female',89,12,'A','Science','2008/11/23'),
+(1014,'Tani Rastogi','Female',82,12,'A','Science','2008/11/23');
+select * from student;
+SELECT StdName, DOB FROM student;
+SELECT * FROM student WHERE percentage >= 80;
+SELECT StdName, Stream, Percentage from student WHERE Percentage > 80;
+SELECT * from student WHERE stream = 'Science' AND Percentage > 75;
